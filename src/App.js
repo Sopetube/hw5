0a2dd5b56b44b8fd767b8cfc8ecd4de5985da1c2
Vlastyslav <sopetube@gmail.com>
2023-08-28 14:58:04 +0300

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import './App.css';
-import { Route, Routes, BrowserRouter, createBrowserRouter,
-  RouterProvider,
+import { Route, Routes, BrowserRouter
  } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SignIn />,
-  },
-  {
-    path: "signup",
-    element: <SignUp />,
-  },
-]);
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "signin",
-//         element: <SignIn />,
-//       },
-//       {
-//         path: "signup",
-//         element: <SignUp />,
-//       },
-//     ]
-//   },
-// ]);
 
 
 function App() {
 
   return (
     <>
-     {/* <BrowserRouter>
+     <BrowserRouter>
      <div className="login">
        <Routes>
-         <Route path='/signin' element={<SignIn/>} />
+         <Route path='/' element={<SignIn/>} />
          <Route path='/signup' element={<SignUp/>} />
        </Routes>
      </div>
-    </BrowserRouter> */}
-    <RouterProvider router={router} />
+    </BrowserRouter>
     </>
   );
 }
