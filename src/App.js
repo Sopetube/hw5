c39752d55e9f04e4c231b80118ef2cca25431a4d
Vlastyslav <sopetube@gmail.com>
2023-08-28 17:39:54 +0300

    nav is added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
           <Link to='signin'>Sign In</Link>
           <Link to='signup'>Sign Up</Link>
         </nav>
-
         <Routes>
           <Route path='signin' element={<SignIn/>} />
           <Route path='signup' element={<SignUp/>} />
