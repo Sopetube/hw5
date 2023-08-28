39be2528c1ca02ca78ab409a7f6d2603484ab605
Vlastyslav <sopetube@gmail.com>
2023-08-28 14:50:01 +0300

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import './App.css';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, createBrowserRouter,
+  RouterProvider,
+ } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignIn />,
+  },
+  {
+    path: "signup",
+    element: <SignUp />,
+  },
+]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "signin",
+//         element: <SignIn />,
+//       },
+//       {
+//         path: "signup",
+//         element: <SignUp />,
+//       },
+//     ]
+//   },
+// ]);
+
 
 function App() {
+
   return (
-    <BrowserRouter>
-    <div className="login">
-      <Routes>
-        {/* <Route path='/' element={<SignIn/>} /> */}
-        <Route path='/signin' element={<SignIn/>} />
-        <Route path='/signup' element={<SignUp/>} />
-      </Routes>
-    </div>
-    </BrowserRouter>
+    <>
+     {/* <BrowserRouter>
+     <div className="login">
+       <Routes>
+         <Route path='/signin' element={<SignIn/>} />
+         <Route path='/signup' element={<SignUp/>} />
+       </Routes>
+     </div>
+    </BrowserRouter> */}
+    <RouterProvider router={router} />
+    </>
   );
 }
 
